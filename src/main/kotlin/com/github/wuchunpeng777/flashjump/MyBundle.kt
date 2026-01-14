6595171488135ce4efa14772d133c3bd49333994
GitHub Action <action@github.com>
2026-01-14 03:09:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/wuchunpeng777/flashjump/MyBundle.kt
+++ src/main/kotlin/com/github/wuchunpeng777/flashjump/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wuchunpeng777.flashjump
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wuchunpeng777/flashjump/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/wuchunpeng777/flashjump/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.wuchunpeng777.flashjump.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
